261b16893cb40a640476d2fc4cad6ed89633b752
AntramZ <58196675+AntramZ@users.noreply.github.com>
2019-11-25 19:29:07 -0600

    Add files via upload

---
+++ Antram_FinalProject/Antram_FinalProject/Form1.Designer.cs
@@ -0,0 +1,197 @@
+﻿namespace Antram_FinalProject
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openFileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.menuStrip1.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(684, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.openFileToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // openFileToolStripMenuItem
+            // 
+            this.openFileToolStripMenuItem.Name = "openFileToolStripMenuItem";
+            this.openFileToolStripMenuItem.Size = new System.Drawing.Size(124, 22);
+            this.openFileToolStripMenuItem.Text = "Open File";
+            this.openFileToolStripMenuItem.Click += new System.EventHandler(this.OpenFile);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(378, 27);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(294, 295);
+            this.richTextBox1.TabIndex = 1;
+            this.richTextBox1.Text = "";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(348, 28);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(24, 294);
+            this.button1.TabIndex = 2;
+            this.button1.Text = ">>>";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Search);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(6, 51);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(273, 20);
+            this.textBox1.TabIndex = 3;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(6, 128);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(273, 20);
+            this.textBox2.TabIndex = 4;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(6, 207);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(273, 20);
+            this.textBox3.TabIndex = 5;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(6, 35);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(55, 13);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Director(s)";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(6, 109);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(80, 13);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Cast Member(s)";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(6, 188);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(80, 13);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Plot Keyword(s)";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.textBox1);
+            this.groupBox1.Controls.Add(this.textBox2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.textBox3);
+            this.groupBox1.Location = new System.Drawing.Point(55, 52);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(287, 255);
+            this.groupBox1.TabIndex = 9;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Search Parameters";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(684, 334);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Margin = new System.Windows.Forms.Padding(2);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem openFileToolStripMenuItem;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.GroupBox groupBox1;
+    }
+}
+
---
+++ Antram_FinalProject/Antram_FinalProject/Form1.cs
@@ -0,0 +1,358 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.OleDb; //Allows us to connect to Access Database
+using System.IO; //Allows access to streamreader and writer commands
+
+namespace Antram_FinalProject
+{
+    public partial class Form1 : Form
+    {
+        //property to hold filepath
+        private string filepath;
+        //Arrays to store multiple search values
+        string[] director;
+        string[] cast;
+        string[] keyword;
+
+        public Form1()
+        {
+            InitializeComponent();
+            filepath = ""; //Set filepath to blank string
+            button1.Enabled = false; //Disable button until a file is selected
+            richTextBox1.Text = "Please select a file\n"; //Prompt user to select a file
+
+            //Check if summary file has been created
+            try
+            {
+                using (var sr = new StreamReader("Antram_Summary.csv"))
+                {
+                    richTextBox1.AppendText("Summary file exists");
+                    sr.Close();
+                }
+            }
+            catch
+            {
+                using(var sw = new StreamWriter("Antram_Summary.csv"))
+                {
+                    sw.WriteLine("filename,castSearch,directorSearch,keywordSearch");
+                    sw.Close();
+                }
+            }
+        }
+
+        private void OpenFile(object sender, EventArgs e)
+        {
+
+            //Opening open file dialog to select file
+            DialogResult dr = openFileDialog1.ShowDialog();
+            if (dr == DialogResult.OK) //Saves selected filepath
+            {
+                filepath = openFileDialog1.FileName;
+                richTextBox1.Clear();
+                button1.Enabled = true; //Re-enable button once file is selected
+            }
+            else //If nothing selected then filepath set to blank string
+            {
+                filepath = "";
+                richTextBox1.Text = "Select a database.";
+                button1.Enabled = false;               
+            }
+        }
+
+        private void Search(object sender, EventArgs e)
+        {
+            //Variable to hold title value for search csv file
+            string searchTitle = String.Format("{0}{1}{2}", textBox1.Text.Trim(' ') ,textBox2.Text.Trim(' ') , textBox3.Text.Trim(' '));
+
+            //Create Search log file
+            using (var sw2 = new StreamWriter(String.Format("Antram_{0}", searchTitle.Trim(' '))))
+            {
+                sw2.WriteLine("RealeaseYear,Title,Director,Cast");
+                sw2.Close();
+            }
+            
+            //Flag variables to check if text boxes are blank initialized to false by default
+            bool directorCheck = false;
+            bool castCheck = false;
+            bool keywordCheck = false;
+
+            //Logic to check if searchbox is blank
+            if (textBox1.Text != "")
+            {
+                director = textBox1.Text.Split(' ');
+                directorCheck = true;
+            }
+
+            if (textBox2.Text != "")
+            {
+
+                cast = textBox2.Text.Split(' ');
+                castCheck = true;
+            }
+
+            if (textBox3.Text != "")
+            {
+                keyword = textBox3.Text.Split(' ');
+                keywordCheck = true;
+            }
+            
+            //Append Search Summary File
+            using (var sw = new StreamWriter("Antram_Summary.csv", true))
+            {
+                sw.WriteLine("Antram_{0},{1},{2},{3}", searchTitle.Trim(' '), textBox2.Text, textBox1.Text, textBox3.Text);
+                sw.Close();
+            }
+
+            //Connection object to make connection to Database
+            string connectionString = String.Format("Provider=Microsoft.ACE.OLEDB.12.0;Data Source={0};", filepath);
+
+            //We need a properly formatted connection string to connect to database
+            OleDbConnection dbconn = new OleDbConnection(connectionString);
+            dbconn.Open(); //Open the connection
+
+            OleDbCommand dbCommand = new OleDbCommand(); //Allows us to execute commands to the reader
+
+            dbCommand.CommandText = "SELECT * FROM MoviePlots";
+            dbCommand.Connection = dbconn;
+
+            OleDbDataReader dbReader = dbCommand.ExecuteReader(); //sends query to database connection to retreive data
+
+            richTextBox1.Clear();
+
+            while (dbReader.Read()) //While still in database
+            {
+                //Logic to determine if a search is complete with only one flagged search
+                
+                if (directorCheck && !castCheck && !keywordCheck)
+                {
+                    //loop to search through directors array and look for matches in file
+                    for (int i = 0; i < director.Length; i++)
+                    {
+                        if (dbReader[4].ToString().Contains(director[i]))
+                        {
+                            richTextBox1.AppendText(String.Format("{0} | {1} | {2} | {3}\n", dbReader[1], dbReader[2], dbReader[4], dbReader[5]));
+                        
+                            //Adding double quotes around multiple search parameters
+                            if (director.Length > 2)
+                            {
+                                //Append Search File
+                                using (var sw2 = new StreamWriter(String.Format("Antram_{0}", searchTitle.Trim(' ')), true))
+                                {
+                                    sw2.WriteLine("{0},{1},\"{2}\",{3}", dbReader[1], dbReader[2], dbReader[4], dbReader[5]);
+                                    sw2.Close();
+                                }
+                            }
+                            else
+                            {
+                                //Append Search File
+                                using (var sw2 = new StreamWriter(String.Format("Antram_{0}", searchTitle.Trim(' ')), true))
+                                {
+                                    sw2.WriteLine("{0},{1},{2},{3}", dbReader[1], dbReader[2], dbReader[4], dbReader[5]);
+                                    sw2.Close();
+                                }
+                            }
+
+                            break;
+                        }
+                    }
+                }
+                else if (castCheck && !directorCheck && !keywordCheck)
+                {
+                    //loop to search through cast array and look for matches in file
+                    for (int i = 0; i < cast.Length; i++)
+                    {
+                        if (dbReader[5].ToString().Contains(cast[i]))
+                        {
+                            richTextBox1.AppendText(String.Format("{0} | {1} | {2} | {3}\n", dbReader[1], dbReader[2], dbReader[4], dbReader[5]));
+
+                            //Adding double quotes around multiple search parameters
+                            if (cast.Length > 2)
+                            {
+                                //Append Search File
+                                using (var sw2 = new StreamWriter(String.Format("Antram_{0}", searchTitle.Trim(' ')), true))
+                                {
+                                    sw2.WriteLine("{0},{1},\"{2}\",{3}", dbReader[1], dbReader[2], dbReader[4], dbReader[5]);
+                                    sw2.Close();
+                                }
+                            }
+                            else
+                            {
+                                //Append Search File
+                                using (var sw2 = new StreamWriter(String.Format("Antram_{0}", searchTitle.Trim(' ')), true))
+                                {
+                                    sw2.WriteLine("{0},{1},{2},{3}", dbReader[1], dbReader[2], dbReader[4], dbReader[5]);
+                                    sw2.Close();
+                                }
+                            }
+
+                            break;
+                        }
+                        
+                    }
+                }
+                else if (keywordCheck && !directorCheck && !castCheck)
+                {
+                    //loop to search through keyword array and look for matches in file
+                    for (int i = 0; i < keyword.Length; i++)
+                    {
+                        if (dbReader[8].ToString().Contains(keyword[i]))
+                        {
+                            richTextBox1.AppendText(String.Format("{0} | {1} | {2} | {3}\n", dbReader[1], dbReader[2], dbReader[4], dbReader[5]));
+
+                            //Adding double quotes around multiple search parameters
+                            if (keyword.Length > 2)
+                            {
+                                //Append Search File
+                                using (var sw2 = new StreamWriter(String.Format("Antram_{0}", searchTitle.Trim(' ')), true))
+                                {
+                                    sw2.WriteLine("{0},{1},\"{2}\",{3}", dbReader[1], dbReader[2], dbReader[4], dbReader[5]);
+                                    sw2.Close();
+                                }
+                            }
+                            else
+                            {
+                                //Append Search File
+                                using (var sw2 = new StreamWriter(String.Format("Antram_{0}", searchTitle.Trim(' ')), true))
+                                {
+                                    sw2.WriteLine("{0},{1},{2},{3}", dbReader[1], dbReader[2], dbReader[4], dbReader[5]);
+                                    sw2.Close();
+                                }
+                            }
+
+                            break;
+                        }
+                        
+                    }
+                }
+                //Logic with 2 valid flags
+                else if (directorCheck && castCheck && !keywordCheck)
+                {
+                    int matchcount = 0; //counts partial matches. When a whole match is reached (2 partials in this case) then print results
+                    for (int i = 0; i < director.Length; i++)
+                    {
+                        if (dbReader[4].ToString().Contains(director[i]))
+                        {
+                            matchcount++;
+                            break;
+                        }
+                    }
+                    for (int i = 0; i < cast.Length; i++)
+                    {
+                        if (dbReader[5].ToString().Contains(cast[i]))
+                        {
+                            matchcount++;
+                            break;
+                        }
+                    }
+
+                    if (matchcount == 2)
+                    {
+                        richTextBox1.AppendText(String.Format("{0} | {1} | {2} | {3}\n", dbReader[1], dbReader[2], dbReader[4], dbReader[5]));
+                    }
+                    
+                }
+                else if (castCheck && keywordCheck && !directorCheck)
+                {
+                    int matchcount = 0; //counts partial matches. When a whole match is reached (2 partials in this case) then print results
+                    for (int i = 0; i < keyword.Length; i++)
+                    {
+                        if (dbReader[8].ToString().Contains(keyword[i]))
+                        {
+                            matchcount++;
+                            break;
+                        }
+                    }
+                    for (int i = 0; i < cast.Length; i++)
+                    {
+                        if (dbReader[5].ToString().Contains(cast[i]))
+                        {
+                            matchcount++;
+                            break;
+                        }
+                    }
+
+                    if (matchcount == 2)
+                    {
+                        richTextBox1.AppendText(String.Format("{0} | {1} | {2} | {3}\n", dbReader[1], dbReader[2], dbReader[4], dbReader[5]));
+                    }
+                   
+                }
+                else if (directorCheck && keywordCheck && !castCheck)
+                {
+                    int matchcount = 0; //counts partial matches. When a whole match is reached (2 partials in this case) then print results
+                    for (int i = 0; i < keyword.Length; i++)
+                    {
+                        if (dbReader[8].ToString().Contains(keyword[i]))
+                        {
+                            matchcount++;
+                            break;
+                        }
+                    }
+                    for (int i = 0; i < director.Length; i++)
+                    {
+                        if (dbReader[4].ToString().Contains(director[i]))
+                        {
+                            matchcount++;
+                            break;
+                        }
+                    }
+
+                    if (matchcount == 2)
+                    {
+                        richTextBox1.AppendText(String.Format("{0} | {1} | {2} | {3}\n", dbReader[1], dbReader[2], dbReader[4], dbReader[5]));
+                    }
+                   
+                }
+                //logic with all 3 flags valid
+                else if (directorCheck && castCheck && keywordCheck)
+                {
+                    int matchcount = 0; //counts partial matches. When a whole match is reached (2 partials in this case) then print results
+                    for (int i = 0; i < director.Length; i++)
+                    {
+                        if (dbReader[4].ToString().Contains(director[i]))
+                        {
+                            matchcount++;
+                            break;
+                        }
+                    }
+                    for (int i = 0; i < cast.Length; i++)
+                    {
+                        if (dbReader[5].ToString().Contains(cast[i]))
+                        {
+                            matchcount++;
+                            break;
+                        }
+                    }
+                    for (int i = 0; i < keyword.Length; i++)
+                    {
+                        if (dbReader[8].ToString().Contains(keyword[i]))
+                        {
+                            matchcount++;
+                            break;
+                        }
+                    }
+
+                    if (matchcount == 3)
+                    {
+                        richTextBox1.AppendText(String.Format("{0} | {1} | {2} | {3}\n", dbReader[1], dbReader[2], dbReader[4], dbReader[5]));
+                    }
+                }
+
+
+
+
+            }
+
+            dbReader.Close(); //Close dbReader so we can reuse
+            dbconn.Close();//Close Connection
+
+        }
+    }
+}
